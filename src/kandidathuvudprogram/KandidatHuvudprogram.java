481d00d5e812eaf2479fa8d0046d1b1e40170072
Server <Server@129.16.192.40>
2014-01-27 11:21:36 +0100

    Import av Markus

--- src/kandidathuvudprogram/KandidatHuvudprogram.java
+++ src/kandidathuvudprogram/KandidatHuvudprogram.java
@@ -4,6 +4,10 @@
  */
 package kandidathuvudprogram;
 
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Server
@@ -14,6 +18,18 @@ public class KandidatHuvudprogram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String fil = "G1_garb_111111-1s.tsf";
+        Import imp = new Import();
+        String data[] = null;
+        try {
+            data = imp.importera(0, 7, fil);
+        } catch (FileNotFoundException ex) {
+            System.out.println("\n\n--Fil ej funnen: " + fil + " --\n\n");
+            Logger.getLogger(KandidatHuvudprogram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for (int i=0; i<data.length; i++){
+            System.out.println(data[i]);
+        }
     }
 }
