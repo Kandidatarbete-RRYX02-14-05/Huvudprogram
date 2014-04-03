7e4ab8ddb1b3d4cf505fda15ce4e04be52ba8902
hellsten <erik.o.hellsten@gmail.com>
2014-04-03 14:22:08 +0200

    Linjexempel

--- src/kandidathuvudprogram/LinearAnnExample.java
+++ src/kandidathuvudprogram/LinearAnnExample.java
@@ -26,7 +26,7 @@ public class LinearAnnExample {
 		{0.9},{2.2},{2.3},{2.3},{3.4},{2.6},{3.2},{4.2}};
 	
 	// Testdata som används i "Eriks test" 
-	static double[] testIn = {0.4,0.2,0.6};
+	static double[] testIn = {0.4,3.2,0.6};
 
 	/*
 	static double[][] sin;
@@ -55,7 +55,7 @@ public class LinearAnnExample {
 		BasicNetwork network = new BasicNetwork();
 		network.addLayer(new BasicLayer(null, false, nbrIn));
 		//network.addLayer(new BasicLayer(5));
-		network.addLayer(new BasicLayer(new ActivationLinear(), true, 1));
+		network.addLayer(new BasicLayer(1));
 		network.getStructure().finalizeStructure();
 		network.reset();
 		
@@ -65,7 +65,7 @@ public class LinearAnnExample {
 		
 		// train the neural network
 		final Train train = new ResilientPropagation(network, trainingSet);
-		train.addStrategy(new RequiredImprovementStrategy(8)); // reset if improve is less than 1% over 5 cycles
+		// train.addStrategy(new RequiredImprovementStrategy(8000)); // reset if improve is less than 1% over 5 cycles
 		int epoch = 1;
 		do {
 			train.iteration();
@@ -78,7 +78,7 @@ public class LinearAnnExample {
 		// test the neural network
 		System.out.println("Neural Network Results:");
 		for(MLDataPair pair: trainingSet ) {
-			final MLData output = network.compute(pair.getInput()); // network.compute(MlData) använder nätverket
+			final MLData output = network.compute(pair.getInput()); // network.compute(MLData) använder nätverket
 			double[] inpArray = pair.getInputArray();	// Skriver ut hela arrayen
 			for(int i = 0;i<nbrIn;i++){
 				System.out.print( " " + inpArray[i]); 
@@ -87,13 +87,11 @@ public class LinearAnnExample {
 			System.out.println("ideal=" + pair.getIdeal().getData(0) + ", actual=" + output.getData(0)); // instanceOfMldata.getData(index i) ger utdata nbr i.
 		}	
 
-		for (int i = 0; i<10; i++){
-
-		}
+		
 		BasicNeuralData ndIn = new BasicNeuralData(testIn);  // Gör basicNeuralDasta av double[] testIn för network.compute behöver MLData för att räkna på. 
 		MLData ndOut = network.compute(ndIn);
 		System.out.println("Eriks test");
-		System.out.println("In: " + ndIn.getData(0) + ", " + ndIn.getData(1) + ", " + ndIn.getData(1));
+		System.out.println("In: " + ndIn.getData(0) + ", " + ndIn.getData(1) + ", " + ndIn.getData(2));
 		System.out.println("Ut: " + ndOut.getData(0));
 	}
 }
