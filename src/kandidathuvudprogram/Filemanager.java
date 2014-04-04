7f94a1809b4e4fee3a1aec92668ecfce2f2a929b
hellsten <erik.o.hellsten@gmail.com>
2014-04-04 19:18:38 +0200

    Filemanager

---
+++ src/kandidathuvudprogram/Filemanager.java
@@ -0,0 +1,70 @@
+package kandidathuvudprogram;
+
+import java.io.File;
+
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.ml.data.buffer.BinaryDataLoader;
+import org.encog.ml.data.buffer.codec.NeuralDataSetCODEC;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+
+public class Filemanager {
+	
+	/**
+	 * 
+	 * @param datum
+	 * @return double[] med gravidata 
+	 */
+	public static double[] readGravFile(String datum){
+		String fil = "gravidata/" + datum.substring(2).replaceAll("-", "") + ".tsf";
+		Import imp = new Import();
+		String dataTime[], dataValue[];  
+
+		dataTime = imp.importWhole(fil);
+		dataValue = new String[dataTime.length];
+		String[] temp;
+		for (int i=0; i<dataTime.length-1; i++){
+			temp = dataTime[i].split(" ");
+			dataValue[i] = temp[temp.length-1];
+			dataTime[i] = temp[0];
+		}
+		
+		double data[] = new double[dataTime.length-1];
+		for (int i=0; i<data.length; i++){
+			data[i] = Double.parseDouble(dataValue[i]);
+		};
+		
+		return data;
+	}
+	
+	/**
+	 * 
+	 * @param set MLDataSet som man skapar bin filen ifrån, ex: "2010-05-10"
+	 * @param outFile
+	 */
+
+	public static void createBin(String datum, double alpha, String win){
+
+		File binFile = new File("Data/Network/" + datum.substring(2).replaceAll("-", "") + ".bin");
+		
+		BasicMLDataSet set = new BasicMLDataSet();
+		
+		PowerSpectrum spectrum = new PowerSpectrum(readGravFile(datum),alpha,win,4);
+
+		for (int i = 0; i<4; i++){
+			set.add(new BasicMLData(spectrum.getRelevantSpectrum()), new BasicMLData(spectrum.getRelevantSpectrum()));
+		}
+		
+		System.out.println("Inputsize:" + set.getInputSize());
+		System.out.println("Idealsize:" + set.getIdealSize());
+		
+		NeuralDataSetCODEC codec = new NeuralDataSetCODEC(set);
+		
+		BinaryDataLoader loader = new BinaryDataLoader(codec);
+		
+		loader.external2Binary(binFile);	
+	}
+
+}
