ba236edd6d747fe2823b31cc194fe0ffb8a6a1ad
hellsten <erik.o.hellsten@gmail.com>
2014-04-11 13:55:00 +0200

    Wavecorrtest byggs ut, Utskrift2Matrix finns

--- src/kandidathuvudprogram/Utskrift.java
+++ src/kandidathuvudprogram/Utskrift.java
@@ -19,5 +19,23 @@ public class Utskrift {
 		  outputWriter.flush();  
 		  outputWriter.close();  
 		}
+	
+	
+	public static void write2Matrix (String filename, double[][] x, String delimiter) throws IOException{
+		  BufferedWriter outputWriter = null;
+		  outputWriter = new BufferedWriter(new FileWriter(filename));
+		  for (int a = 0; a<x.length; a++){
+			  for (int i = 0; i < x[a].length; i++) {
+			    // Maybe:
+			   // outputWriter.write(x[i]+"");
+			    // Or:
+			    outputWriter.write(Double.toString(x[a][i]));
+			    outputWriter.write(delimiter);
+			  }
+			  outputWriter.newLine();
+		  }
+		  outputWriter.flush();  
+		  outputWriter.close();  
+		}
 
 }
