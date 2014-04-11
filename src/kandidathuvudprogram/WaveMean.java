f3e7ffdbb1a8521e04f33b2973cf9dcf93ecb12d
davsven <davsven@o7152a-07.o7152a.studat.a.1.hb.cdal.chalmers.se>
2014-04-11 18:28:17 +0200

    Lagat WaveMean så den fungerar!

--- src/kandidathuvudprogram/WaveMean.java
+++ src/kandidathuvudprogram/WaveMean.java
@@ -19,72 +19,84 @@ public class WaveMean {
     public static void main(String[] arg){
         int xRawSize = 85*8;
         int yRawSize = 35*8;
-        String[] dataString = new String[xRawSize*yRawSize]; 
-        Import imp = new Import(); 
-        
-        dataString = imp.importWhole("wavedata/raw_20140105_00.tsv"); 
         double[][] dataDouble = new double[yRawSize][xRawSize];
-        for(int n = 0; n < yRawSize; n++){
-            for(int m = 0; m < xRawSize; m++) {
-                dataDouble[n][m] = Double.parseDouble(dataString[n*xRawSize + m]);
-            }
-        }
+        importAndFormat("wavedata/raw_20140105_00.tsv");
+       
         
         double[][] dataMean = new double[35][85];
         dataMean = calculateWaveMean(1,dataDouble);
         
+        try{                                //Sparar doubleArray till CSV
+            Utskrift.write2Matrix("wavedata/test.csv",dataMean,"," );
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        
         
     }
     
+    /** 
+     * Beräknar medelvärdet av rutor med storleken 1 kvadratgrad i en 2D double-array. 
+     * @param res Upplösning i grader. Används ej , alltid 1. 
+     * @param waveData 2D double array vars medelvärde ska beräknas. Ska ha storleken 8*(35x80).
+     * @return 2D double-array med storleken 35x80 där varje punkt är medelvärdet av "rutan". 
+     */
+    
     public static double[][] calculateWaveMean(int res, double[][] waveData){
         
-        res = 1;
+        res = 1; // Kanske orkar fixa andra upplösningar senare
         int[] range = {-70,15, 35, 70 }; // Förbestämt område, ändra ej!
         
         int xLength = 85/res;
         int yLength = 35/res;
-        double[][] tempData = new double[2][xLength];
+        int[] count =  {0,0,0};
+        double[][] tempData = new double[2][xLength]; // Temporär fil för att beräkna medelvärde
         double[][] meanWaveData = new double[yLength][xLength];
- 
-        
-        waveData = expandDoubleArray(res, waveData); //
-        
 
-        
-        for(int a = 0;a < waveData.length/(8*res); a = a + 8*res){ // Alla for-loopar är i termer av index i expandDoubleArray
+        for(int a = 0;a < (waveData.length); a = a + 8*res){ // Alla for-loopar är i termer av index i expandDoubleArray
             for(int n = a; n < a +8*res; n++){
-               for(int b = 0; b < waveData[0].length/(8*res); b = b + 8*res ){
+               for(int b = 0; b < waveData[0].length; b = b + 8*res ){
                    for(int m = b; m < b + 8*res; m++){
                    
-                       if (waveData[n][m] != -1){
-                           tempData[0][b/(8*res)] = tempData[0][b/(8*res)] + waveData[n][m]; //Summa våghöjd 
-                           tempData[1][b/(8*res)] = tempData[1][b/(8*res)] + 1 ; // Antal datapunkter
-                       }
-                               
-                       
-                       
-                   }
-                   
-                   
-               }
-                
+                       if (waveData[n][m] != -1){ //Om det finns mätdata i punkten
+                           tempData[0][b/(8*res)] = tempData[0][b/(8*res)] + waveData[n][m]; //Summa våghöjd i rutan
+                           tempData[1][b/(8*res)] = tempData[1][b/(8*res)] + 1 ; // Antal datapunkter i rutan
+                           
+                        }   
+                    } 
+                }
             }
-            for(int i = 0;i < xLength; i++ ){
-                
+            for(int i = 0;i < tempData[1].length; i++ ){
+               
                 if(tempData[1][i] > 0){
-                    meanWaveData[a/(8*res)][i] = tempData[0][a]/tempData[1][a];
+                    meanWaveData[a/(8*res)][i] = tempData[0][i]/tempData[1][i];
+                    
                 }else{
                         meanWaveData[a/(8*res)][i] = -1; // Om det inte finns någon data inom området
-                        
                         }
+                }
+            for(int j = 0; j < tempData[1].length; j++ ){ //Nollställ temp
+                tempData[0][j] = 0; 
+                tempData[1][j] = 0;        
             }
+            
+            
         }
         
         
         return meanWaveData; 
     }
 
+    /** 
+     * Expanderar en 2D double-array med marginal för att förhindra fel vid uträkning av medelvärde. De nya elmenten har värdet -1. 
+     * @param res Upplösning i grader. Används ej , alltid 1. 
+     * @param dArray 2D double-array med data.
+     * @return dArray med marginaler
+     */
+    
     public static double[][] expandDoubleArray(int res,  double[][] dArray){
+        res = 1;
         
         double[][] expandedArray = new double[dArray.length + 8*res -1][dArray[0].length + 8*res -1];
         
@@ -94,16 +106,44 @@ public class WaveMean {
             }
         }
         
-        for(int n = 4*res - 1; n < expandedArray.length -4*res ; n++){ // Fyller mitten av array med input
-            for(int m = 4*res - 1; m < expandedArray.length - 4*res; m++){
-            
-                expandedArray[n][m] = dArray[n -4*res +1][m -4*res +1];
+        for(int n = 0; n < dArray.length ; n++){ // Fyller mitten av array med input
+            for(int m = 0; m < dArray[n].length ; m++){
+                
+                expandedArray[n+res*4 -1][m+res*4 -1] = dArray[n][m];
+                
             }
         
         }
         
         return expandedArray;
         
+    }
+    /**
+     * Importerar en 1-vektor med data och skriver den som en 2D double-array.
+     * @param file Filens sökväg och namn
+     * @return En 2D double-array med storleken (rawYSize*rawXSize) 
+     */
+    
+    //public static double[][] importAndFormat(String file, int rawXSize, int rawYSize){
+    public static double[][] importAndFormat(String file){
+         
+        
+        int rawXSize = 85*8;
+        int rawYSize = 35*8;
+        
+        
+        String[] dataString = new String[rawXSize*rawYSize]; 
+        Import imp = new Import(); 
+        
+        dataString = imp.importWhole(file); //Läser fil till doubleArray
+        double[][] dataDouble = new double[rawYSize][rawXSize];
+        for(int n = 0; n < rawYSize; n++){
+            for(int m = 0; m < rawXSize; m++) {
+                dataDouble[n][m] = Double.parseDouble(dataString[n*(rawXSize+1) + m]);
+            }
+        }
+        
+        return dataDouble;
     }
 }
 
